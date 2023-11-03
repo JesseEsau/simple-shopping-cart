7771e8999c0751f94433ce3482c571fd65fc542a
Esau Jesse <esaujesse@yahoo.com>
2023-11-03 18:11:06 +0100

    simple shopping cart without any functionaluty

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { AllRoutes } from "./routes/AllRoutes";
+import { Header } from "./components";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <AllRoutes />
+    </div>
+  )
+}
+
+export default App
